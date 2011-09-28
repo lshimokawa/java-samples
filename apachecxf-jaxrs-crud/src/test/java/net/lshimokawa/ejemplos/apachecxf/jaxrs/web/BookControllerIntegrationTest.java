577292af081d9278d404837091d8790013ae90c7
Lennon Shimokawa <lshimokawa@gmail.com>
2011-09-28 16:22:33 -0500

    get integration testing

--- apachecxf-jaxrs-crud/src/test/java/net/lshimokawa/ejemplos/apachecxf/jaxrs/web/BookControllerIntegrationTest.java
+++ apachecxf-jaxrs-crud/src/test/java/net/lshimokawa/ejemplos/apachecxf/jaxrs/web/BookControllerIntegrationTest.java
@@ -1,7 +1,7 @@
 package net.lshimokawa.ejemplos.apachecxf.jaxrs.web;
 
-import junit.framework.Assert;
-
+import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +12,24 @@ import org.springframework.web.client.RestTemplate;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/applicationContext-test.xml")
 public class BookControllerIntegrationTest {
+	private Logger logger = Logger
+			.getLogger(BookControllerIntegrationTest.class);
 
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@Test
-	public void test() {
-		String result = restTemplate.getForObject(
-				"http://localhost:8080/apachecxf-jaxrs-crud/services/books/",
-				String.class);
-		Assert.assertNotNull(result);
+	public void testGetBook() {
+		String response = restTemplate
+				.getForObject(
+						"http://localhost:8080/apachecxf-jaxrs-crud/services/books/{isbn}",
+						String.class, "0201699699");
+		logger.debug("testGetBook: " + response);
+		Assert.assertNotNull(response);
+	}
+
+	@Test
+	public void testPost() {
+		// TODO
 	}
 }
